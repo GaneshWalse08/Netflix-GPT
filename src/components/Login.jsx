6d904e9ab40c21c0e48f4839e7673e8be32ff801
Ganesh Walse <ganeshwalse1975@gmail.com>
2026-06-03 13:54:44 +0530

    SignIn/SignUp Form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,23 +1,59 @@
+import { useState } from "react";
 import { signInBackgroundImg } from "../utils/constants";
 import Header from "./Header";
 
 const Login = () => {
-  return <div>
-    <Header/>
-    <div className="absolute -z-10" >
-      <img src={signInBackgroundImg}></img>
-    </div>
+  const [isSignIn, setisSignIn] = useState(true);
 
-    <div className="absolute inset-0 flex justify-center items-center">
+  const handleSignUp = () => {
+    setisSignIn(!isSignIn);
+  }
 
-      <form className="p-12 bg-black/75 w-3/12 ">
-      <h2 className="text-white font-bold text-2xl pb-6">Sign In</h2>
-      <input type="email" placeholder="Enter your Email" className="mb-4 p-2 w-full bg-gray-800 text-white"/>
-      <input type="password" placeholder="Enter your Password" className="mb-4 p-2 w-full bg-gray-800 text-white"/>
-      <button className="bg-red-700 p-1.5 w-full cursor-pointer my-4">Sign In</button>
-    </form>
+  return (
+    <div>
+      <Header />
+      <div className="absolute -z-10">
+        <img src={signInBackgroundImg}></img>
+      </div>
+
+      <div className="absolute inset-0 flex justify-center items-center">
+        <form className="p-12 bg-black/75 w-3/12 ">
+          <h2 className="text-white font-bold text-2xl pb-6">{isSignIn ? "Sign In" : "Sign Up"}</h2>
+
+          {!isSignIn && <input
+            type="text"
+            placeholder="Username"
+            className="mb-4 p-2 w-full bg-gray-800 text-white"
+          />}
+          <input
+            type="email"
+            placeholder="Email"
+            className="mb-4 p-2 w-full bg-gray-800 text-white"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="mb-4 p-2 w-full bg-gray-800 text-white"
+          />
+          <button className="bg-red-700 p-1.5 w-full cursor-pointer my-4">
+            {isSignIn ? "Sign In" : "Sign Up"}
+          </button>
+
+          {isSignIn ?  <p className="text-white">
+            New to Netflix?{" "}
+            <span className="text-blue-400 cursor-pointer underline" onClick={handleSignUp}>
+              Sign Up Here.
+            </span>
+          </p> : <p className="text-white">
+            Already Registered?{" "}
+            <span className="text-blue-400 cursor-pointer underline" onClick={handleSignUp}>
+              Sign In Here.
+            </span>
+          </p>}
+        </form>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default Login;
